46529328c1663d6fada113c1d94e22ec54ab9f2d
ryan-munger <112397901+ryan-munger@users.noreply.github.com>
2024-03-23 11:15:08 -0400

    page functionality done

--- js/home.js
+++ js/home.js
@@ -1,3 +1,9 @@
 document.getElementById("getStartedButton").addEventListener("click", function() {
     window.location.href = "games.html";
 });
+
+document.getElementById("lucky").addEventListener("click", function() {
+    let pages = ["goFish.html", "blackjack.html", "poker.html"];
+    let randomNumber = Math.floor(Math.random() * pages.length);
+    window.location.href = pages[randomNumber];
+});
